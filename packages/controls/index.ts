506f736d478f90a7b7848e56aac4d564164ba5f6
lebedevki <lebedevki@lad24.ru>
2022-05-30 09:51:54 +0300

    chore: import improvement

--- packages/controls/index.ts
+++ packages/controls/index.ts
@@ -1,6 +1,1 @@
-import Radio from './src/components/Radio/Radio';
-
-export * from './src/components/Radio/types';
-export * from './src/components/types';
-
-export {Radio};
+export * from './src/components';
