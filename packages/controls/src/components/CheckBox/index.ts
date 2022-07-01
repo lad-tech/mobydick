a03f591e0be46555b298d8a0986961535f99abb1
gubanikhinda <gubanikhinda@lad24.ru>
2022-07-01 11:04:19 +0300

    feat: checkbox
    added checkbox

---
+++ packages/controls/src/components/CheckBox/index.ts
@@ -0,0 +1,5 @@
+import CheckBox from './CheckBox';
+
+export * from './types';
+
+export {CheckBox};
--- packages/styles/src/icons/index.ts
+++ packages/styles/src/icons/index.ts
@@ -2,6 +2,7 @@ import Show from './svg/show.svg';
 import Hide from './svg/hide.svg';
 import Loader from './svg/loader.svg';
 import Plus from './svg/plus.svg';
+import Check from './svg/check.svg';
 import Exit from './svg/exit.svg';
 
-export {Show, Hide, Loader, Plus, Exit};
+export {Show, Hide, Loader, Plus, Check, Exit};
