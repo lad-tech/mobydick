b3308cabb2215f69f89289bf39e183b9b2efc1fa
Kirill Lebedev <lebedevki@lad24.ru>
2022-07-14 12:58:31 +0300

    style: add BgWhiteSoft color

--- packages/styles/src/colors/types.ts
+++ packages/styles/src/colors/types.ts
@@ -1,3 +1,15 @@
-import {defaultTextLightColor} from './color';
+import {
+  defaultTextLightColor,
+  defaultBgDarkColor,
+  defaultIconLightColor,
+  defaultElementLightColor,
+  defaultBorderLightColor,
+  defaultCTALightColor,
+} from './color';
 
 export type ITextColors = typeof defaultTextLightColor;
+export type IIconColors = typeof defaultIconLightColor;
+export type IElementColors = typeof defaultElementLightColor;
+export type IBorderColors = typeof defaultBorderLightColor;
+export type IBgColors = typeof defaultBgDarkColor;
+export type ICTAColors = typeof defaultCTALightColor;
