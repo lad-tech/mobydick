7c9606d65550d41402b946ea73ebe1abe38a55c9
Bibazavr <lebedevki@lad24.ru>
2022-07-29 12:30:26 +0300

    fix(popups): add customization for ModalBase container

--- packages/popups/src/components/ModalBase/ModalBase.tsx
+++ packages/popups/src/components/ModalBase/ModalBase.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {View} from '@npm/mobydick-core';
 import {useStyles} from '@npm/mobydick-styles';
+import {StyleProp, ViewStyle} from 'react-native';
 
 import stylesCreate from '../PopupBase/stylesCreate';
 import {PopupBase} from '../PopupBase';
@@ -10,17 +11,22 @@ import CloseIcon from './CloseIcon';
 import DescriptionText from './DescriptionText';
 import Title from './Title';
 
-const ModalBase: FC<Omit<IPopup, 'Content'> & {onClose: () => void}> & {
+const ModalBase: FC<
+  Omit<IPopup, 'Content'> & {
+    onClose: () => void;
+    containerStyle?: StyleProp<ViewStyle>;
+  }
+> & {
   Title: typeof Title;
   CloseIcon: typeof CloseIcon;
   DescriptionText: typeof DescriptionText;
 } = props => {
-  const {children, overlayStyle, onClose} = props;
+  const {children, overlayStyle, onClose, containerStyle} = props;
   const [styles] = useStyles(stylesCreate);
 
   return (
     <PopupBase onClose={onClose} overlayStyle={overlayStyle}>
-      <View style={[styles.container]}>{children}</View>
+      <View style={[styles.container, containerStyle]}>{children}</View>
     </PopupBase>
   );
 };
