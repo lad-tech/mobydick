8bd0cda1bca70780ebd13cd4ef39ffee04467ee3
vpsmolina <SmolinaVP@lad24.ru>
2022-08-09 15:39:59 +0300

    fix(Button): deleted view

--- storybook/stories/cta/Button/Button.stories.tsx
+++ storybook/stories/cta/Button/Button.stories.tsx
@@ -3,22 +3,19 @@ import React from 'react';
 import {boolean, number, select, text} from '@storybook/addon-knobs';
 import {action} from '@storybook/addon-actions';
 import {Button, ISize, ITypes} from '@npm/mobydick-cta';
-import {View} from '@npm/mobydick-core';
 
 import CenterView from '../../CenterView';
 
 storiesOf('Design System/CTA/Button', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
   .add('basic', () => (
-    <View>
-      <Button
-        text={text('text', 'text big text')}
-        onPress={action('onPress')}
-        type={select('type', ITypes, ITypes.primary)}
-        disabled={boolean('disabled', false)}
-        loading={boolean('loading', false)}
-        size={select('size', ISize, ISize.small)}
-        style={{minWidth: number('minWidth', 0)}}
-      />
-    </View>
+    <Button
+      text={text('text', 'text big text')}
+      onPress={action('onPress')}
+      type={select('type', ITypes, ITypes.primary)}
+      disabled={boolean('disabled', false)}
+      loading={boolean('loading', false)}
+      size={select('size', ISize, ISize.small)}
+      style={{minWidth: number('minWidth', 0)}}
+    />
   ));
