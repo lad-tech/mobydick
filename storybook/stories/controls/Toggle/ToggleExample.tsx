4e6adf091342eb43985391ec3fce82ebdf3d56f8
vpsmolina <SmolinaVP@lad24.ru>
2022-09-15 11:24:06 +0300

    fix(toggle): add props needsOffscreenAlphaCompositing, for fix android opacity

--- storybook/stories/controls/Toggle/ToggleExample.tsx
+++ storybook/stories/controls/Toggle/ToggleExample.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Toggle} from '@npm/mobydick-controls';
 import {boolean} from '@storybook/addon-knobs';
 
 const ToggleExample = () => {
+  const [isActive, setActive] = useState(true);
+
   return (
     <Toggle
-      active={boolean('active', false)}
+      onPress={() => setActive(!isActive)}
+      active={isActive}
       disabled={boolean('disabled', false)}
     />
   );
