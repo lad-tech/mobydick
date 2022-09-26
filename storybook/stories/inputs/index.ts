b4a021b6dbc8ce614c6c2f2da61f1a0becd38642
vpsmolina <SmolinaVP@lad24.ru>
2022-09-26 11:07:01 +0300

    chore(inputs): почистила index

--- storybook/stories/inputs/index.ts
+++ storybook/stories/inputs/index.ts
@@ -1,5 +1,1 @@
-import './InputField/ExampleInput';
-import './InputField/ExampleInputWithSubtitleIcon';
 import './DropDown/DropDown.stories';
-import './Search/ExampleSearch';
-import './Inputs.stories';
