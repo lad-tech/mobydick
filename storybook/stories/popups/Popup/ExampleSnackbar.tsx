8a378c2c23cb2862a5a5f99de697f21f8b2dd4dd
vpsmolina <SmolinaVP@lad24.ru>
2022-10-06 17:25:58 +0300

    feature(snackbar): доработки в snackbar

--- storybook/stories/popups/Popup/ExampleSnackbar.tsx
+++ storybook/stories/popups/Popup/ExampleSnackbar.tsx
@@ -1,4 +1,4 @@
-import {IContentProps, SnackbarBase} from '@npm/mobydick-popups';
+import {IContentProps, IPosition, SnackbarBase} from '@npm/mobydick-popups';
 import React, {FC} from 'react';
 import {boolean, select, text} from '@storybook/addon-knobs';
 import {rem, SimpleIcon, useTheme} from '@npm/mobydick-styles';
@@ -7,36 +7,33 @@ import {Typography} from '@npm/mobydick-typography';
 
 import selectFont from '../../../utils/selectFont';
 
-enum IPlacement {
-  start = 'flex-start',
-  center = 'center',
-  end = 'flex-end',
-}
-
 const ExampleSnackbar: FC<IContentProps> = props => {
   const {colors} = useTheme();
   const {onClose} = props;
-
-  const placement = select('Placement', IPlacement, IPlacement.end);
+  const position = select('Position', IPosition, IPosition.bottom);
 
   return (
     <SnackbarBase
       {...props}
-      overlayStyle={{
-        justifyContent: placement,
-      }}>
+      containerStyle={{alignItems: 'flex-start'}}
+      position={position}
+      timeShow={2000}>
       {boolean('show icon', true) && (
         <SimpleIcon
-          name={'icon-check'}
-          color={colors.IconAdditional}
+          name={'icon-noconnection'}
+          color={colors.IconMuted}
           style={{
             paddingRight: rem(8),
           }}
         />
       )}
       <SnackbarBase.Title
-        title={text('title', 'Подборка добавлена')}
-        titleFont={select('Snackbar font', selectFont, 'Regular-Contrast-XS')}
+        title={text(
+          'title',
+          'Для перехода нужен интернет. \n' +
+            'Когда подключение появится, HUAWEI Wallet станет доступен',
+        )}
+        titleFont={select('Snackbar font', selectFont, 'Medium-Contrast-XS')}
       />
       {boolean('show right button ', true) && (
         <TouchableOpacity onPress={onClose}>
