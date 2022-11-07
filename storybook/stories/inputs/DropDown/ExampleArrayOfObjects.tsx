69cf1add7d52728177af4cf65730d81b8e72cc77
Ksenia Petrova <petrovakd@lad24.ru>
2022-11-07 14:00:19 +0600

    chore(drop down): removed unnessesary settings

--- storybook/stories/inputs/DropDown/ExampleArrayOfObjects.tsx
+++ storybook/stories/inputs/DropDown/ExampleArrayOfObjects.tsx
@@ -1,16 +1,8 @@
 import React, {useState} from 'react';
 import {DropDown, IInputsTypes, IListItem} from '@npm/mobydick-inputs';
-import {boolean, number, select, text} from '@storybook/addon-knobs';
-import {
-  iconNames,
-  rem,
-  SimpleIcon,
-  SimpleIconName,
-  useTheme,
-} from '@npm/mobydick-styles';
+import {number, select, text} from '@storybook/addon-knobs';
+import {rem} from '@npm/mobydick-styles';
 import {Typography} from '@npm/mobydick-typography';
-
-import selectFont from '../../../utils/selectFont';
 
 type ILanguage = {
   id: number;
@@ -20,13 +12,6 @@ type ILanguage = {
 
 const ExampleArrayOfObjects = () => {
   const [selected, setSelected] = useState<ILanguage>();
-
-  const {colors} = useTheme();
-  const subtitleIconName = select(
-    'subtitleIcon name',
-    iconNames,
-    'icon-arrow-down',
-  );
 
   const listObject: IListItem<ILanguage>[] = [
     {label: 'Русский', value: {id: 1, code: 'ru', name: 'Русский'}},
@@ -44,46 +29,12 @@ const ExampleArrayOfObjects = () => {
         }
         placeholder={text('placeholder', 'Выберите язык')}
         label={text('title', 'Язык')}
-        disabled={boolean('disabled', false)}
         type={select('type', IInputsTypes, IInputsTypes.default)}
-        subtitle={text('subtitle', 'Подпись')}
-        subtitleIcon={
-          boolean('show subtitleIcon', false) ? subtitleIconName : undefined
-        }
-        rightIcon={
-          <SimpleIcon
-            name={
-              select(
-                'icon name',
-                iconNames,
-                'icon-arrow-down',
-              ) as SimpleIconName
-            }
-            color={colors.IconMuted}
-          />
-        }
         buttonStyle={{
           height: number('button height', rem(40)),
           width: number('button width', rem(300)),
         }}
         flatListStyle={{width: number('list width', rem(300))}}
-        buttonTextFont={select('button font', selectFont, 'Regular-Muted-M')}
-        labelFont={select('label font', selectFont, 'Medium-Tertiary-XS')}
-        flatListTextFont={select(
-          'flatlist font',
-          selectFont,
-          'Regular-Secondary-M',
-        )}
-        buttonTextFontChosen={select(
-          'button chosen font',
-          selectFont,
-          'Medium-Primary-M',
-        )}
-        flatListTextFontPressed={select(
-          'button pressed font',
-          selectFont,
-          'Medium-Primary-M',
-        )}
       />
       <Typography>{JSON.stringify(selected, null, 2)}</Typography>
     </>
