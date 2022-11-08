702c72fb4265d8cb4303cd3ae0240bee264331c9
vpsmolina <SmolinaVP@lad24.ru>
2022-11-08 19:44:03 +0300

    chore(toggle): add animation for toggle

--- packages/controls/src/components/Toggle/Toggle.tsx
+++ packages/controls/src/components/Toggle/Toggle.tsx
@@ -1,34 +1,61 @@
-import React, {FC} from 'react';
-import {Pressable} from '@npm/mobydick-core';
-import {Animated} from 'react-native';
+import React, {FC, useRef} from 'react';
+import {Animated, PanResponder} from 'react-native';
 import {rem, useStyles} from '@npm/mobydick-styles';
 
 import stylesCreate from './stylesCreate';
-import useToggle from './useToggle';
 import {IToggle} from './types';
 
-const Toggle: FC<IToggle> = ({active, disabled, ...rest}) => {
+const Toggle: FC<IToggle> = ({active, disabled}) => {
   const [styles, theme] = useStyles(stylesCreate, disabled);
-  const toggle = useToggle(active);
+  // const toggle = useToggle(active);
+  const pan = useRef(new Animated.Value(active ? 20 : -20)).current;
 
-  const translateX = toggle.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, rem(20)],
-  });
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
 
-  const backgroundColor = toggle.interpolate({
-    inputRange: [0, 1],
+      onPanResponderMove: Animated.event([null, {dx: pan}], {
+        useNativeDriver: false,
+      }),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      },
+    }),
+  ).current;
+
+  const backgroundColor = pan.interpolate({
+    inputRange: [-20, 20],
     outputRange: [theme.colors.ElementMuted, theme.colors.ElementBase],
+    extrapolate: 'clamp',
   });
+
+  const translateX = pan.interpolate({
+    inputRange: [-20, 20],
+    outputRange: [0, rem(20)],
+    extrapolate: 'clamp',
+  });
+  //
+  // const backgroundColor = toggle.interpolate({
+  //   inputRange: [0, 1],
+  //   outputRange: [theme.colors.ElementMuted, theme.colors.ElementBase],
+  // });
+  //
+  // const translateX = pan.interpolate({
+  //   inputRange: [0, 1],
+  //   outputRange: [0, rem(20)],
+  // });
 
   return (
-    <Pressable disabled={disabled} {...rest}>
+    // <Pressable disabled={disabled} {...rest}>
+    <Animated.View
+      style={[styles.container, {backgroundColor}]}
+      needsOffscreenAlphaCompositing={true}>
       <Animated.View
-        style={[styles.container, {backgroundColor}]}
-        needsOffscreenAlphaCompositing={true}>
-        <Animated.View style={[styles.switcher, {transform: [{translateX}]}]} />
-      </Animated.View>
-    </Pressable>
+        style={[styles.switcher, {transform: [{translateX}]}]}
+        {...panResponder.panHandlers}
+      />
+    </Animated.View>
+    // </Pressable>
   );
 };
 
