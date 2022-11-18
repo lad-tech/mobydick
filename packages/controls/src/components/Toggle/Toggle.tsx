5eb72d666ad239f3169ad786f7cd7db306dcd69a
vpsmolina <SmolinaVP@lad24.ru>
2022-11-18 11:30:11 +0300

    chore(toggle): new attempts

--- packages/controls/src/components/Toggle/Toggle.tsx
+++ packages/controls/src/components/Toggle/Toggle.tsx
@@ -8,32 +8,72 @@ import {IToggle} from './types';
 const Toggle: FC<IToggle> = ({active, disabled}) => {
   const [styles, theme] = useStyles(stylesCreate, disabled);
   // const toggle = useToggle(active);
-  const pan = useRef(new Animated.Value(active ? 20 : -20)).current;
+  const pan = useRef(new Animated.Value(active ? 30 : -30)).current;
 
+  const leftPos = -30;
+  const rightPos = 30;
   const panResponder = useRef(
     PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
+      // onStartShouldSetPanResponder: () => true,
+      // onPanResponderStart: event => {
+      //   const touches = event.nativeEvent.touches;
+      //   console.log('onPanResponderStart nativeEvent', event.nativeEvent);
+      //   if (touches.length === 1) {
+      //     if (pan._value === leftPos) {
+      //       console.log('onPanResponderStart pan._value === -20');
+      //       Animated.spring(pan, {
+      //         toValue: rightPos,
+      //         useNativeDriver: false,
+      //       }).start();
+      //     } else if (pan._value === rightPos) {
+      //       console.log('onPanResponderStart pan._value === 20');
+      //       Animated.spring(pan, {
+      //         toValue: leftPos,
+      //         useNativeDriver: false,
+      //       }).start();
+      //     }
+      //   }
+      // },
 
       onPanResponderMove: Animated.event([null, {dx: pan}], {
         useNativeDriver: false,
       }),
-      onPanResponderRelease: () => {
-        pan.flattenOffset();
-      },
+      // onPanResponderRelease: (evt, gestureState) => {
+      //   pan.flattenOffset();
+      //   console.log('onPanResponderRelease', gestureState);
+      //
+      //   if (pan._value < 0) {
+      //     console.log('onPanResponderRelease pan._value < 0');
+      //     Animated.spring(pan, {
+      //       toValue: leftPos,
+      //       useNativeDriver: false,
+      //     }).start();
+      //   } else if (pan._value >= 0) {
+      //     console.log('onPanResponderRelease pan._value > 0');
+      //     Animated.spring(pan, {
+      //       toValue: rightPos,
+      //       useNativeDriver: false,
+      //     }).start();
+      //   }
+      // },
     }),
   ).current;
 
   const backgroundColor = pan.interpolate({
-    inputRange: [-20, 20],
+    inputRange: [leftPos, rightPos],
     outputRange: [theme.colors.ElementMuted, theme.colors.ElementBase],
     extrapolate: 'clamp',
   });
 
+  console.log('pan', pan);
+
   const translateX = pan.interpolate({
-    inputRange: [-20, 20],
+    inputRange: [leftPos, rightPos],
     outputRange: [0, rem(20)],
     extrapolate: 'clamp',
   });
+
   //
   // const backgroundColor = toggle.interpolate({
   //   inputRange: [0, 1],
@@ -49,11 +89,9 @@ const Toggle: FC<IToggle> = ({active, disabled}) => {
     // <Pressable disabled={disabled} {...rest}>
     <Animated.View
       style={[styles.container, {backgroundColor}]}
-      needsOffscreenAlphaCompositing={true}>
-      <Animated.View
-        style={[styles.switcher, {transform: [{translateX}]}]}
-        {...panResponder.panHandlers}
-      />
+      needsOffscreenAlphaCompositing={true}
+      {...panResponder.panHandlers}>
+      <Animated.View style={[styles.switcher, {transform: [{translateX}]}]} />
     </Animated.View>
     // </Pressable>
   );
