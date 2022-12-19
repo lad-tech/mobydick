b84209a4021fbafd09920aaf1258c2fdfe8516c1
vpsmolina <SmolinaVP@lad24.ru>
2022-12-19 14:37:28 +0300

    fix(dropdown): replace height from safe-area-context

--- packages/inputs/src/components/DropDown/__tests__/DropDown.test.tsx
+++ packages/inputs/src/components/DropDown/__tests__/DropDown.test.tsx
@@ -4,6 +4,7 @@ import {SimpleIcon} from '@npm/mobydick-styles';
 import {PopupsProvider} from '@npm/mobydick-popups';
 import {View} from 'react-native';
 import Constants from '@npm/mobydick-popups/src/components/PopupBase/constants';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import DropDown from '../DropDown';
 import {ACCESSIBILITY_LABEL} from '../constants';
@@ -23,16 +24,18 @@ describe('@npm/mobydick-inputs/DropDown', () => {
 
   it('renders correctly 10 elements', () => {
     const {toJSON} = render(
-      <PopupsProvider>
-        <DropDown
-          selectedItem={list[1]}
-          placeholder={'Выберите язык'}
-          title={'Название поля'}
-          list={list}
-          onPress={jest.fn()}
-          rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
-        />
-      </PopupsProvider>,
+      <SafeAreaProvider>
+        <PopupsProvider>
+          <DropDown
+            selectedItem={list[1]}
+            placeholder={'Выберите язык'}
+            title={'Название поля'}
+            list={list}
+            onPress={jest.fn()}
+            rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
+          />
+        </PopupsProvider>
+      </SafeAreaProvider>,
     );
     expect(toJSON()).toMatchSnapshot();
   });
@@ -62,17 +65,19 @@ describe('@npm/mobydick-inputs/DropDown', () => {
       );
 
     const {getByLabelText} = render(
-      <PopupsProvider>
-        <DropDown
-          placeholder={'Выберите язык'}
-          title={'Название поля'}
-          list={array}
-          onPress={onPress}
-          type={IInputsTypes.disabled}
-          selectedItem={array[1].label}
-          rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
-        />
-      </PopupsProvider>,
+      <SafeAreaProvider>
+        <PopupsProvider>
+          <DropDown
+            placeholder={'Выберите язык'}
+            title={'Название поля'}
+            list={array}
+            onPress={onPress}
+            type={IInputsTypes.disabled}
+            selectedItem={array[1].label}
+            rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
+          />
+        </PopupsProvider>
+      </SafeAreaProvider>,
     );
     const pressable = getByLabelText(ACCESSIBILITY_LABEL.selector);
     fireEvent.press(pressable);
@@ -102,24 +107,26 @@ describe('@npm/mobydick-inputs/DropDown', () => {
       );
 
     const {getByLabelText, toJSON, getByTestId} = render(
-      <PopupsProvider>
-        <DropDown
-          selectedItem={'selectedItem'}
-          placeholder={'Выберите язык'}
-          title={'Название поля'}
-          list={array}
-          onPress={onPress}
-          subtitle={'subtitle'}
-          subtitleIcon={'icon-account'}
-          buttonStyle={{
-            width: 400,
-            height: 70,
-            borderColor: '#000',
-            backgroundColor: '#000',
-          }}
-          rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
-        />
-      </PopupsProvider>,
+      <SafeAreaProvider>
+        <PopupsProvider>
+          <DropDown
+            selectedItem={'selectedItem'}
+            placeholder={'Выберите язык'}
+            title={'Название поля'}
+            list={array}
+            onPress={onPress}
+            subtitle={'subtitle'}
+            subtitleIcon={'icon-account'}
+            buttonStyle={{
+              width: 400,
+              height: 70,
+              borderColor: '#000',
+              backgroundColor: '#000',
+            }}
+            rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
+          />
+        </PopupsProvider>
+      </SafeAreaProvider>,
     );
     const pressable = getByLabelText(ACCESSIBILITY_LABEL.selector);
 
@@ -137,91 +144,101 @@ describe('@npm/mobydick-inputs/DropDown', () => {
 
   it('renders correctly 10 elements type wrong', () => {
     const {toJSON} = render(
-      <PopupsProvider>
-        <DropDown
-          selectedItem={'selectedItem'}
-          placeholder={'Выберите язык'}
-          title={'Название поля'}
-          list={list}
-          onPress={jest.fn()}
-          type={IInputsTypes.wrong}
-          titleFont={'Medium-Tertiary-XS'}
-          titleStyle={{paddingBottom: 8}}
-          buttonStyle={{
-            width: 400,
-            height: 70,
-            borderColor: '#000',
-            backgroundColor: '#000',
-          }}
-          rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
-        />
-      </PopupsProvider>,
+      <SafeAreaProvider>
+        <PopupsProvider>
+          <DropDown
+            selectedItem={'selectedItem'}
+            placeholder={'Выберите язык'}
+            title={'Название поля'}
+            list={list}
+            onPress={jest.fn()}
+            type={IInputsTypes.wrong}
+            titleFont={'Medium-Tertiary-XS'}
+            titleStyle={{paddingBottom: 8}}
+            buttonStyle={{
+              width: 400,
+              height: 70,
+              borderColor: '#000',
+              backgroundColor: '#000',
+            }}
+            rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
+          />
+        </PopupsProvider>
+      </SafeAreaProvider>,
     );
     expect(toJSON()).toMatchSnapshot();
   });
   it('renders correctly 10 elements type valid', () => {
     const {toJSON} = render(
-      <PopupsProvider>
-        <DropDown
-          selectedItem={'selectedItem'}
-          placeholder={'Выберите язык'}
-          title={'Название поля'}
-          list={list}
-          onPress={jest.fn()}
-          type={IInputsTypes.valid}
-          rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
-        />
-      </PopupsProvider>,
+      <SafeAreaProvider>
+        <PopupsProvider>
+          <DropDown
+            selectedItem={'selectedItem'}
+            placeholder={'Выберите язык'}
+            title={'Название поля'}
+            list={list}
+            onPress={jest.fn()}
+            type={IInputsTypes.valid}
+            rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
+          />
+        </PopupsProvider>
+      </SafeAreaProvider>,
     );
     expect(toJSON()).toMatchSnapshot();
   });
   it('renders correctly 10 elements disabled', () => {
     const {toJSON} = render(
-      <PopupsProvider>
-        <DropDown
-          selectedItem={undefined}
-          placeholder={'Выберите язык'}
-          title={'Название поля'}
-          list={list}
-          onPress={jest.fn()}
-          disabled={true}
-          rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
-        />
-      </PopupsProvider>,
+      <SafeAreaProvider>
+        <PopupsProvider>
+          <DropDown
+            selectedItem={undefined}
+            placeholder={'Выберите язык'}
+            title={'Название поля'}
+            list={list}
+            onPress={jest.fn()}
+            disabled={true}
+            rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
+          />
+        </PopupsProvider>
+      </SafeAreaProvider>,
     );
     expect(toJSON()).toMatchSnapshot();
   });
   it('renders correctly without array', () => {
     const {toJSON} = render(
-      <PopupsProvider>
-        <DropDown
-          selectedItem={list[1]}
-          placeholder={'Выберите язык'}
-          title={'Название поля'}
-          required={false}
-          list={[]}
-          onPress={jest.fn()}
-          rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
-        />
-      </PopupsProvider>,
+      <SafeAreaProvider>
+        <PopupsProvider>
+          <DropDown
+            selectedItem={list[1]}
+            placeholder={'Выберите язык'}
+            title={'Название поля'}
+            required={false}
+            list={[]}
+            onPress={jest.fn()}
+            rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
+          />
+        </PopupsProvider>
+      </SafeAreaProvider>,
     );
     expect(toJSON()).toMatchSnapshot();
   });
   it('renders correctly without array with text and font', () => {
     const {toJSON} = render(
-      <PopupsProvider>
-        <DropDown
-          selectedItem={list[1]}
-          placeholder={'Выберите язык'}
-          title={'Название поля'}
-          required={true}
-          listEmptyFont={'Regular-Error-L'}
-          listEmptyText={'Ничего нет'}
-          list={[]}
-          onPress={jest.fn()}
-          rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
-        />
-      </PopupsProvider>,
+      <SafeAreaProvider>
+        <PopupsProvider>
+          <DropDown
+            selectedItem={list[1]}
+            placeholder={'Выберите язык'}
+            title={'Название поля'}
+            required={true}
+            listEmptyFont={'Regular-Error-L'}
+            listEmptyText={'Ничего нет'}
+            list={[]}
+            onPress={jest.fn()}
+            rightIcon={<SimpleIcon name={'icon-arrow-down'} />}
+          />
+        </PopupsProvider>
+      </SafeAreaProvider>,
     );
     expect(toJSON()).toMatchSnapshot();
   });
