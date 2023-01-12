baf6ef01766823381753b2b517539eb6deb1a0f3
vpsmolina <SmolinaVP@lad24.ru>
2023-01-12 08:52:33 +0300

    chore(dots): + test

---
+++ packages/core/src/other/components/Dots/__tests__/stylesCreate.test.ts
@@ -0,0 +1,26 @@
+import {renderHook} from '@testing-library/react-hooks';
+
+import stylesCreate from '../stylesCreate';
+import useTheme from '../../../../styles/theme/hooks/useTheme';
+import rem from '../../../../styles/spaces/rem';
+
+describe('dots/stylesCreate', () => {
+  it('must return', () => {
+    const {result} = renderHook(() => useTheme());
+
+    expect(stylesCreate(result.current)).toEqual({
+      dot: {
+        width: rem(8),
+        height: rem(8),
+        marginHorizontal: 5,
+        borderRadius: rem(8) / 2,
+        backgroundColor: '#9BA1B0',
+      },
+      dots: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 5,
+      },
+    });
+  });
+});
