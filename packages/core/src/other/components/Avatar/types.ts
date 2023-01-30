53dd9c9bc02d657ecf34a0396e4950f9db0acf93
semenovamm <semenovamm@lad24.ru>
2023-01-30 17:21:38 +0300

    feat: add avatar size

--- packages/core/src/other/components/Avatar/types.ts
+++ packages/core/src/other/components/Avatar/types.ts
@@ -2,3 +2,16 @@ export interface IUser {
   logo: string; // https://vraki.net/sites/default/files/inline/images/30_55.jpg
   name: string; // "Иван Пушкин"
 }
+
+export enum ISizeAvatar {
+  S = 'S',
+  M = 'M',
+  L = 'L',
+  XL = 'XL',
+}
+
+interface IAvatarProps {
+  user: IUser | null;
+  size?: ISizeAvatar;
+}
+export type AvatarProps = IAvatarProps;
