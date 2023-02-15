ab00b3d326f880035a814a2561f20f2c71c87780
vpsmolina <SmolinaVP@lad24.ru>
2023-02-15 11:02:44 +0300

    feat(toggle): add useIsForeground

--- packages/utils/src/hooks/index.ts
+++ packages/utils/src/hooks/index.ts
@@ -2,5 +2,6 @@ import useThrottle from './useThrottle';
 import useDebounce from './useDebounce';
 import useLatest from './useLatest';
 import useToggle from './useToggle';
+import useIsForeground from './useIsForeground';
 
-export {useThrottle, useDebounce, useLatest, useToggle};
+export {useThrottle, useDebounce, useLatest, useToggle, useIsForeground};
