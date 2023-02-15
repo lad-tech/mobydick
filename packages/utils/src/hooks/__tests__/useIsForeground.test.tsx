b4c2bb9d856c8885e71ec1bf52f27c7a56ddfe0a
vpsmolina <SmolinaVP@lad24.ru>
2023-02-15 11:28:15 +0300

    chore(useIsForeground): add tests

--- packages/utils/src/hooks/__tests__/useIsForeground.test.tsx
+++ packages/utils/src/hooks/__tests__/useIsForeground.test.tsx
@@ -1,12 +1,28 @@
-import {renderHook} from '@testing-library/react-hooks';
+import {act, renderHook} from '@testing-library/react-hooks';
+import {AppState} from 'react-native';
 
 import useIsForeground from '../useIsForeground';
 
 describe('useIsForeground', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
   test('positive case', () => {
     const {
       result: {current},
     } = renderHook(() => useIsForeground());
     expect(current).toBe(true);
+  });
+  test('negative case', () => {
+    const appStateSpy = jest.spyOn(AppState, 'addEventListener');
+
+    const {result} = renderHook(() => useIsForeground());
+
+    const [firstCall] = appStateSpy.mock.calls;
+    const [, cb] = firstCall!;
+
+    act(() => cb('inactive'));
+
+    expect(result.current).toBe(false);
   });
 });
