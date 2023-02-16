c0430d120b9ccfa25e2f1050a46c2d0ad9c4ab0f
vpsmolina <SmolinaVP@lad24.ru>
2023-02-16 12:47:41 +0300

    chore(Toggle): delete
    BREAKING CHANGES(toggle): delete Toggle

--- storybook/stories/controls/index.ts
+++ storybook/stories/controls/index.ts
@@ -1,6 +1,5 @@
 import './Radio/RadioExample';
 import './Checkbox/CheckboxExample';
-import './Toggle/ToggleExample';
 import './ControlsLists/RadioListExample';
 import './ControlsLists/CheckboxListExample';
 import './Controls.stories';
