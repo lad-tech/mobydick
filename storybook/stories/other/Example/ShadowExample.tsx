5f5659fd74faf018dfb360d05e0f7b24d82ec089
vpsmolina <SmolinaVP@lad24.ru>
2023-03-13 15:36:00 +0300

    chore(Shadow): add for test shadow

---
+++ storybook/stories/other/Example/ShadowExample.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {number} from '@storybook/addon-knobs';
+
+import {
+  IThemeContext,
+  rem,
+  SimpleIcon,
+  useStyles,
+  useTheme,
+  View,
+} from '@npm/mobydick-core';
+
+const ShadowExample = () => {
+  const [styles] = useStyles(stylesCreate);
+  const {colors} = useTheme();
+  const elevation = number('android elevation', 1);
+  const shadowOpacity = number('ios shadowOpacity', 0.5);
+  const shadowRadius = number('ios shadowRadius', 4.27);
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.shadow,
+          styles.box,
+          {
+            elevation,
+            shadowOpacity,
+            shadowRadius,
+          },
+        ]}
+      />
+      <View
+        style={[
+          styles.shadow,
+          styles.box,
+          {borderRadius: rem(20)},
+          {
+            elevation,
+            shadowOpacity,
+            shadowRadius,
+          },
+        ]}
+      />
+      <View
+        style={[
+          styles.shadow,
+          styles.circle,
+          {
+            elevation,
+            shadowOpacity,
+            shadowRadius,
+          },
+        ]}
+      />
+      <View
+        style={[
+          styles.shadow,
+          {
+            elevation,
+            shadowOpacity,
+            shadowRadius,
+          },
+        ]}>
+        <SimpleIcon
+          name={'icon-starfill'}
+          size={100}
+          color={colors.BgPrimary}
+        />
+      </View>
+    </View>
+  );
+};
+
+const stylesCreate = ({colors, spaces}: IThemeContext) =>
+  StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    shadow: {
+      marginVertical: spaces.Space10,
+      backgroundColor: 'transparent',
+
+      shadowOffset: {width: 0, height: 1},
+    },
+    box: {
+      width: rem(200),
+      height: rem(100),
+      backgroundColor: colors.BgPrimary,
+    },
+    circle: {
+      width: rem(100),
+      height: rem(100),
+      borderRadius: rem(50),
+      backgroundColor: colors.BgPrimary,
+    },
+  });
+
+export default ShadowExample;
