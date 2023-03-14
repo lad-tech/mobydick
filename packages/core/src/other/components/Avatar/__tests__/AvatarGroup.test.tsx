590cad656b53c9a9e17b3af61ec896f5fd2e4a48
vpsmolina <SmolinaVP@lad24.ru>
2023-03-14 08:33:53 +0300

    fix(Avatar): limit in count group

--- packages/core/src/other/components/Avatar/__tests__/AvatarGroup.test.tsx
+++ packages/core/src/other/components/Avatar/__tests__/AvatarGroup.test.tsx
@@ -4,20 +4,7 @@ import React from 'react';
 import AvatarGroup from '../AvatarGroup';
 import {IAvatarTypes} from '../types';
 
-const smallGroupDate = [
-  {
-    firstName: 'Иван',
-    lastName: 'Пушкин',
-  },
-  {
-    firstName: 'Иван',
-    lastName: 'Пушкин',
-  },
-  {
-    logo: 'https://vraki.net/sites/default/files/inline/images/30_55.jpg',
-    firstName: 'Иван',
-    lastName: 'Пушкин',
-  },
+const defaultGroupDate = [
   {
     firstName: 'Иван',
     lastName: 'Пушкин',
@@ -27,46 +14,50 @@ const smallGroupDate = [
     lastName: 'Пушкин',
   },
 ];
-const bigGroupDate = [
-  {
-    firstName: 'Иван',
-    lastName: 'Пушкин',
-  },
-  {
-    firstName: 'Иван',
-    lastName: 'Пушкин',
-  },
+const smallGroupDate = [
   {
     logo: 'https://vraki.net/sites/default/files/inline/images/30_55.jpg',
     firstName: 'Иван',
     lastName: 'Пушкин',
   },
-  {
-    firstName: 'Иван',
-    lastName: 'Пушкин',
-  },
-  {
-    firstName: 'Иван',
-    lastName: 'Пушкин',
-  },
-  {
-    firstName: 'Иван',
-    lastName: 'Пушкин',
-  },
+  ...defaultGroupDate,
+];
+
+const defaultUser = [
   {
     firstName: 'Иван',
     lastName: 'Пушкин',
   },
 ];
 describe('AvatarGroup', () => {
+  test('render AvatarGroup < 3', () => {
+    const {toJSON} = render(
+      <AvatarGroup
+        groups={defaultGroupDate}
+        backgroundColor={'#ff0000'}
+        type={IAvatarTypes.text}
+      />,
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
   test('render AvatarGroup', () => {
-    const {toJSON} = render(<AvatarGroup groups={smallGroupDate} />);
+    const {toJSON} = render(
+      <AvatarGroup
+        groups={smallGroupDate.concat(Array(8).fill(defaultUser))}
+      />,
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+  test('render AvatarGroup = 4', () => {
+    const {toJSON} = render(
+      <AvatarGroup groups={defaultGroupDate.concat(defaultGroupDate)} />,
+    );
     expect(toJSON()).toMatchSnapshot();
   });
   test('render AvatarGroup with props', () => {
     const {toJSON} = render(
       <AvatarGroup
-        groups={bigGroupDate}
+        groups={smallGroupDate.concat(Array(101).fill(defaultUser))}
         backgroundColor={'#ff0000'}
         type={IAvatarTypes.text}
       />,
