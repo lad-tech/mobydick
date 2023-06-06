99301b42860b0977c0b280b70855856f01a12236
vpsmolina <SmolinaVP@lad24.ru>
2023-06-06 12:32:45 +0300

    fix(Carousel): return checkScroll and initScroll in Carousel

--- packages/core/src/other/components/Carousel/LoopCarousel.tsx
+++ packages/core/src/other/components/Carousel/LoopCarousel.tsx
@@ -1,58 +1,21 @@
-import React, {useCallback, useRef, useState} from 'react';
-import {FlatList, NativeScrollEvent, NativeSyntheticEvent} from 'react-native';
-import {useSafeAreaFrame} from 'react-native-safe-area-context';
-
-import rem from '../../../styles/spaces/rem';
+import React, {useCallback, useState} from 'react';
 
 import {ICarouselAlign, ICarouselProps} from './types';
 import Carousel from './Carousel';
 
 const LoopCarousel = <T,>({
   data,
-  sideMargin = rem(10),
   itemWidth,
   align = ICarouselAlign.start,
   ...otherProps
-}: Omit<ICarouselProps<T>, 'keyExtractor' | 'autoScroll'>): JSX.Element => {
-  const ref = useRef<FlatList>(null);
+}: Omit<
+  ICarouselProps<T>,
+  'keyExtractor' | 'autoScroll' | 'animateAutoScroll'
+>): JSX.Element => {
   const [infinityData, setInfinityData] = useState([...data, ...data, ...data]);
-
-  const widthSnap = itemWidth + sideMargin * 2;
-  const widthData = widthSnap * data.length;
-  const {width: WIDTH} = useSafeAreaFrame();
-  const emptySpace = WIDTH - widthSnap;
-
-  const initScroll = useCallback(() => {
-    ref.current?.scrollToOffset({
-      offset:
-        align === ICarouselAlign.center
-          ? widthData - emptySpace / 2
-          : widthData,
-      animated: false,
-    });
-  }, [align, widthData, emptySpace]);
-
   const keyExtractorDefault = useCallback((item: T, index?: number) => {
     return String(index) + String(item);
   }, []);
-
-  const checkScroll = useCallback(
-    ({contentOffset}: NativeScrollEvent) => {
-      if (!contentOffset.x) {
-        ref.current?.scrollToOffset({
-          offset: widthData,
-          animated: false,
-        });
-      }
-    },
-    [widthData],
-  );
-  const onScroll = useCallback(
-    (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-      checkScroll(event.nativeEvent);
-    },
-    [checkScroll],
-  );
 
   const onEndReached = useCallback(
     () => setInfinityData([...infinityData, ...data]),
@@ -61,15 +24,13 @@ const LoopCarousel = <T,>({
 
   return (
     <Carousel
-      onLayout={initScroll}
       data={infinityData}
       keyExtractor={keyExtractorDefault}
       itemWidth={itemWidth}
-      onScroll={onScroll}
       onEndReached={onEndReached}
       initialNumToRender={data.length + 5}
       align={align}
-      sideMargin={sideMargin}
+      indexScroll={data.length}
       {...otherProps}
     />
   );
