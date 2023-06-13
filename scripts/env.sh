3aa0bd7a759761d28db3e3cdd2f640f0a43e482b
Bibazavr <lebedevki@lad24.ru>
2023-06-13 07:17:28 +0300

    build: update env script

--- scripts/env.sh
+++ scripts/env.sh
@@ -8,10 +8,16 @@ else
   ENV_DEV=$ENV_DEV
 fi
 
+if [ -z "${ENV_PROD}" ]; then
+  ENV_PROD='{"isProd": true}'
+else
+  ENV_PROD=$ENV_PROD
+fi
+
 if [ "${CI_COMMIT_BRANCH}" = 'master' ]; then
-  echo $ENV_PROD > ./env.json
-else 
-  echo $ENV_DEV > ./env.json
+  echo "$ENV_PROD" > ./env.json
+else
+  echo "$ENV_DEV" > ./env.json
 fi
 
 
