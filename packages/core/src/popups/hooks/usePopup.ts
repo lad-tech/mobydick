958315dc626728e3b9076bcd3ce5c3f2e4ec2021
Aleksandr <ikonnikovAI@lad24.ru>
2023-12-12 20:50:11 +0600

    refactor: types for usePopup

--- packages/core/src/popups/hooks/usePopup.ts
+++ packages/core/src/popups/hooks/usePopup.ts
@@ -3,14 +3,14 @@ import {FC, useRef} from 'react';
 import {IContentProps, IPopup} from '../types';
 import {MobyDickPopup} from '../MobyDickPopup';
 
-const usePopup = <P extends IContentProps = IContentProps>(modal: FC<P>) => {
+const usePopup = <P>(modal: FC<P & IContentProps>) => {
   const id = useRef<string | null>(null);
 
   return {
-    open: (props: IPopup['props'] = {}) => {
+    open: (props?: IPopup<P>['props']) => {
       id.current = MobyDickPopup.openPopup({
         Content: modal as FC<IContentProps>,
-        props: props,
+        props: props || {},
       });
     },
     close: () => {
