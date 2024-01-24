f6e0c5b878685692e1dd623d048e08f0d67a15f6
vpsmolina <smolinavp@lad24.ru>
2024-01-24 11:29:59 +0300

    fix(InputField): changed styles

--- packages/core/src/inputs/components/InputField/InputField.tsx
+++ packages/core/src/inputs/components/InputField/InputField.tsx
@@ -108,7 +108,7 @@ const InputField = forwardRef<ITextInput, IInputFieldsProps>((props, ref) => {
         <TextInput
           ref={ref}
           testID={Constants.testID}
-          style={[styles.textInput, getHeight(), getStyle, style]}
+          style={[styles.textInput, getStyle, style]}
           placeholderTextColor={theme.colors.TextMuted}
           editable={!disabled}
           numberOfLines={1}
