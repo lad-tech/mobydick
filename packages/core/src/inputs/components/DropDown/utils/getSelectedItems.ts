7eb41b49598b6b06815aa37428cb3a01f28b8a1b
Aleksandr <ikonnikovAI@lad24.ru>
2024-02-13 09:53:37 +0600

    refactor: change getSelectedItems function

--- packages/core/src/inputs/components/DropDown/utils/getSelectedItems.ts
+++ packages/core/src/inputs/components/DropDown/utils/getSelectedItems.ts
@@ -8,10 +8,12 @@ function getSelectedItems<T extends IListItem>(
     return [item];
   }
 
-  const element = selectedItems.find(({value}) => value === item.value);
+  const itemAlreadyExist = selectedItems.some(
+    ({value}) => value === item.value,
+  );
 
-  if (element) {
-    return selectedItems.filter(({value}) => element.value !== value);
+  if (itemAlreadyExist) {
+    return selectedItems.filter(({value}) => item.value !== value);
   }
 
   return [...selectedItems, item];
