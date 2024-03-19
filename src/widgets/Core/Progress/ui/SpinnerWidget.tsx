aed43989375e7b7a59b8f5c7b1e84933a8facda5
gubanikhinda <gubanikhinda@lad24.ru>
2024-03-19 09:56:56 +0300

    docs(progress): added some examples to app

--- src/widgets/Core/Progress/ui/SpinnerWidget.tsx
+++ src/widgets/Core/Progress/ui/SpinnerWidget.tsx
@@ -1,10 +1,23 @@
-import {Spinner, Typography, View} from 'shared/ui';
+import {StyleSheet} from 'react-native';
+
+import {ISizeSpinner, Spinner, Typography, View} from 'shared/ui';
 
 export const SpinnerWidget = () => {
   return (
-    <View>
+    <>
       <Typography font={'Regular-Primary-H5'}>Spinner</Typography>
-      <Spinner />
-    </View>
+      <View style={styles.container}>
+        <Spinner size={ISizeSpinner.L} duration={1000} />
+        <Spinner fill={'#000000'} />
+        <Spinner />
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+});
