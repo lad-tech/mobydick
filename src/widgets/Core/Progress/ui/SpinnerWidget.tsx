10fe18f299573a0e26ba7c4905d86acf2ac31dda
gubanikhinda <gubanikhinda@lad24.ru>
2024-03-19 11:32:47 +0300

    docs(progress): added some examples to app

--- src/widgets/Core/Progress/ui/SpinnerWidget.tsx
+++ src/widgets/Core/Progress/ui/SpinnerWidget.tsx
@@ -1,15 +1,19 @@
 import {StyleSheet} from 'react-native';
 
-import {ISizeSpinner, Spinner, Typography, View} from 'shared/ui';
+import {ISizeSpinner, Spinner, Typography, useTheme, View} from 'shared/ui';
 
 export const SpinnerWidget = () => {
+  const {colors} = useTheme();
   return (
     <>
       <Typography font={'Regular-Primary-H5'}>Spinner</Typography>
       <View style={styles.container}>
+        <Spinner size={ISizeSpinner.XS} />
+        <Spinner size={ISizeSpinner.XXS} />
+        <Spinner size={ISizeSpinner.S} />
         <Spinner size={ISizeSpinner.L} duration={1000} />
-        <Spinner fill={'#000000'} />
-        <Spinner />
+        <Spinner size={ISizeSpinner.M} />
+        <Spinner fill={colors.BgContrast} />
       </View>
     </>
   );
