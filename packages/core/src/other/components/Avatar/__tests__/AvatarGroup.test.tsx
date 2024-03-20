1c934fd7e3c20dac0e01c08d645683060c310711
vpsmolina <smolinavp@lad24.ru>
2024-03-20 08:41:01 +0300

    test(Avatar): returned test's data

--- packages/core/src/other/components/Avatar/__tests__/AvatarGroup.test.tsx
+++ packages/core/src/other/components/Avatar/__tests__/AvatarGroup.test.tsx
@@ -2,17 +2,39 @@ import {render} from '@testing-library/react-native';
 
 import AvatarGroup from '../AvatarGroup';
 import {IAvatarTypes} from '../types';
-import {
-  defaultGroupDateTest,
-  defaultUserTest,
-  smallGroupDateTest,
-} from '../../../../../../../src/shared/lib/test/data/avatar';
+
+const defaultGroupDate = [
+  {
+    firstName: 'Иван',
+    lastName: 'Пушкин',
+  },
+  {
+    firstName: 'Иван',
+    lastName: 'Пушкин',
+  },
+];
+
+const smallGroupDate = [
+  {
+    logo: 'https://vraki.net/sites/default/files/inline/images/30_55.jpg',
+    firstName: 'Иван',
+    lastName: 'Пушкин',
+  },
+  ...defaultGroupDate,
+];
+
+const defaultUser = [
+  {
+    firstName: 'Иван',
+    lastName: 'Пушкин',
+  },
+];
 
 describe('AvatarGroup', () => {
   test('render AvatarGroup < 3', () => {
     const {toJSON} = render(
       <AvatarGroup
-        groups={defaultGroupDateTest}
+        groups={defaultGroupDate}
         backgroundColor={'#ff0000'}
         type={IAvatarTypes.text}
       />,
@@ -22,23 +44,21 @@ describe('AvatarGroup', () => {
   test('render AvatarGroup', () => {
     const {toJSON} = render(
       <AvatarGroup
-        groups={smallGroupDateTest.concat(Array(8).fill(defaultUserTest))}
+        groups={smallGroupDate.concat(Array(8).fill(defaultUser))}
       />,
     );
     expect(toJSON()).toMatchSnapshot();
   });
   test('render AvatarGroup = 4', () => {
     const {toJSON} = render(
-      <AvatarGroup
-        groups={defaultGroupDateTest.concat(defaultGroupDateTest)}
-      />,
+      <AvatarGroup groups={defaultGroupDate.concat(defaultGroupDate)} />,
     );
     expect(toJSON()).toMatchSnapshot();
   });
   test('render AvatarGroup with props', () => {
     const {toJSON} = render(
       <AvatarGroup
-        groups={smallGroupDateTest.concat(Array(101).fill(defaultUserTest))}
+        groups={smallGroupDate.concat(Array(101).fill(defaultUser))}
         backgroundColor={'#ff0000'}
         type={IAvatarTypes.text}
       />,
@@ -48,7 +68,7 @@ describe('AvatarGroup', () => {
   test('render AvatarGroup groupCount', () => {
     const {toJSON} = render(
       <AvatarGroup
-        groups={smallGroupDateTest.concat(Array(101).fill(defaultUserTest))}
+        groups={smallGroupDate.concat(Array(101).fill(defaultUser))}
         backgroundColor={'#ff0000'}
         type={IAvatarTypes.text}
         groupCount={50}
