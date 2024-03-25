3ebd066f226525eceb34d2b9eeb7d135fdd789a7
vpsmolina <smolinavp@lad24.ru>
2024-03-25 11:47:15 +0300

    docs(ModalBse): docs and example

--- src/pages/MainStack/Home/Core/Popup/ui/index.tsx
+++ src/pages/MainStack/Home/Core/Popup/ui/index.tsx
@@ -1,9 +1,10 @@
 import {ScrollView, useStyles} from 'shared/ui';
 import getScreenStyles from 'shared/styles/getScreenStyles';
 import {ActionSheetWidget} from 'widgets/Core/Popup/ui/ActionSheetWidget';
-import {ModalWidget} from 'widgets/Core/Popup/ui/ModalWidget';
+import {ModalsWidget} from 'widgets/Core/Popup/ui/ModalsWidget';
 import {SnackbarWidget} from 'widgets/Core/Popup/ui/SnackbarWidget';
 import {TooltipWidget} from 'widgets/Core/Popup/ui/TooltipWidget';
+import {ModalWidget} from 'widgets/Core/Popup/ui/ModalWidget';
 
 const PopupScreen = () => {
   const [styles] = useStyles(getScreenStyles);
@@ -11,9 +12,10 @@ const PopupScreen = () => {
   return (
     <ScrollView contentContainerStyle={[styles.container, {flex: undefined}]}>
       <ActionSheetWidget />
-      <ModalWidget />
+      <ModalsWidget />
       <SnackbarWidget />
       <TooltipWidget />
+      <ModalWidget />
     </ScrollView>
   );
 };
