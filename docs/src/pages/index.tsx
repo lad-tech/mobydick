51802286c98d5a72ee08a6e37a5a959a53d71dbd
Bibazavr <lebedevki@lad24.ru>
2024-03-26 11:59:53 +0300

    docs: fix build
    Signed-off-by: Bibazavr <lebedevki@lad24.ru>

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -15,7 +15,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/components/Intro">
+            to="docs/components/Intro">
             Mobydick Componets
           </Link>
         </div>
