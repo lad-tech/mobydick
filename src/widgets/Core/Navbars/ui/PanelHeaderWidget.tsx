a200c694f2b370df96641e077c6afd0c3e7db835
vpsmolina <smolinavp@lad24.ru>
2024-03-28 13:00:30 +0300

    docs(PanelHeader): added docs and example

---
+++ src/widgets/Core/Navbars/ui/PanelHeaderWidget.tsx
@@ -0,0 +1,20 @@
+import {createStyles, PanelHeader, SimpleIcon, useStyles} from 'shared/ui';
+
+export const PanelHeaderWidget = () => {
+  const [styles] = useStyles(stylesFn);
+
+  return (
+    <PanelHeader
+      title={'PanelHeader'}
+      leftView={<SimpleIcon name={'icon-arrow-left'} />}
+      rightView={<SimpleIcon name={'icon-camera'} />}
+      containerStyle={styles.container}
+    />
+  );
+};
+
+const stylesFn = createStyles(({colors}) => ({
+  container: {
+    backgroundColor: colors.BgAccent,
+  },
+}));
