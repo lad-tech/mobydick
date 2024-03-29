ec4fb9d1216908da2c5f084d807d3575caf51a82
Bibazavr <lebedevki@lad24.ru>
2024-03-29 12:44:14 +0300

    feat: update icons
    Signed-off-by: Bibazavr <lebedevki@lad24.ru>

--- src/pages/MainStack/Home/Core/Styles/ui/index.tsx
+++ src/pages/MainStack/Home/Core/Styles/ui/index.tsx
@@ -1,16 +1,25 @@
-import {useStyles, View} from 'shared/ui';
+import {FlatList, useStyles, View} from 'shared/ui';
 import getScreenStyles from 'shared/styles/getScreenStyles';
 import {ShadowWidget} from 'widgets/Core/Styles/ui/ShadowWidget';
 import {SimpleIconWidget} from 'widgets/Core/Styles/ui/SimpleIconWidget';
+
+const renderItem = () => <View />;
 
 const StylesScreen = () => {
   const [styles] = useStyles(getScreenStyles);
 
   return (
-    <View style={styles.container}>
-      <ShadowWidget />
-      <SimpleIconWidget />
-    </View>
+    <FlatList
+      data={[]}
+      contentContainerStyle={[styles.container, {flex: undefined}]}
+      renderItem={renderItem}
+      ListFooterComponent={
+        <>
+          <ShadowWidget />
+          <SimpleIconWidget />
+        </>
+      }
+    />
   );
 };
 
