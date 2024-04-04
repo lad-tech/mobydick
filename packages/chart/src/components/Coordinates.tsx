d342ded2f476bfd932b432654be4a128d0924bfc
Bibazavr <lebedevki@lad24.ru>
2024-04-04 14:34:27 +0300

    chore: move CoordY and CoordX
    Signed-off-by: Bibazavr <lebedevki@lad24.ru>

--- packages/chart/src/components/Coordinates.tsx
+++ packages/chart/src/components/Coordinates.tsx
@@ -1,127 +1,14 @@
-import {Group, Path, SkFont, Skia, Text} from '@shopify/react-native-skia';
+import {Group, Path, SkFont} from '@shopify/react-native-skia';
 import {SharedValue, useDerivedValue} from 'react-native-reanimated';
 import {IThemeContext} from '@lad-tech/mobydick-core';
 
 import {IFormatter} from '../types';
 import {generateCoordinatesPath} from '../utils/generateCoordinatesPath';
-import {
-  getCoordinateValues,
-  IGetCoordinateValuesReturn,
-} from '../utils/getCoordinateValues';
+import {getCoordinateValues} from '../utils/getCoordinateValues';
 
-interface ICoordYProps {
-  font: SkFont;
-  coords: SharedValue<IGetCoordinateValuesReturn>;
-  coordinateValueYMaxLength: SharedValue<number>;
-  colors: IThemeContext['colors'];
-  index: number;
-}
+import {CoordY} from './CoordY';
+import {CoordX} from './CoordX';
 
-const CoordY = ({
-  font,
-  coordinateValueYMaxLength,
-  coords,
-  index,
-  colors,
-}: ICoordYProps) => {
-  const coordinate = useDerivedValue(() => {
-    const coordinate = coords.value.coordinateValuesY[index]?.coordinate ?? 0;
-
-    return coordinate;
-  });
-
-  const value = useDerivedValue(() => {
-    const value = coords.value.coordinateValuesY[index]?.value ?? '';
-
-    return value;
-  });
-  const path = useDerivedValue(() => {
-    const path =
-      coords.value.coordinateValuesY[index]?.path ?? Skia.Path.Make();
-
-    return path;
-  });
-
-  const x = useDerivedValue(() => {
-    const {width} = font.measureText(value.value);
-    const symbolSize = width / value.value.length;
-    const r = -coordinateValueYMaxLength.value * symbolSize;
-
-    if (r < 0) {
-      return 0;
-    }
-
-    return r;
-  });
-
-  return (
-    <Group>
-      <Path
-        path={path}
-        style="stroke"
-        strokeJoin="round"
-        color={colors.BorderNormal}
-      />
-      <Text
-        font={font}
-        text={value}
-        y={coordinate}
-        x={x}
-        color={colors.TextMuted}
-      />
-    </Group>
-  );
-};
-
-interface ICoordXProps {
-  font: SkFont;
-  coords: SharedValue<IGetCoordinateValuesReturn>;
-  colors: IThemeContext['colors'];
-  index: number;
-  size: SharedValue<{height: number; width: number}>;
-}
-
-const CoordX = ({font, size, coords, index, colors}: ICoordXProps) => {
-  const path = useDerivedValue(() => {
-    const path =
-      coords.value.coordinateValuesX[index]?.path ?? Skia.Path.Make();
-
-    return path;
-  });
-  const value = useDerivedValue(() => {
-    const value = coords.value.coordinateValuesX[index]?.value ?? '';
-
-    return value;
-  });
-
-  const coordinate = useDerivedValue(() => {
-    const coordinate = coords.value.coordinateValuesX[index]?.coordinate ?? 0;
-
-    return coordinate;
-  });
-
-  const y = useDerivedValue(() => {
-    return size.value.height;
-  });
-
-  return (
-    <Group>
-      <Path
-        path={path}
-        style="stroke"
-        strokeJoin="round"
-        color={colors.BorderNormal}
-      />
-      <Text
-        font={font}
-        text={value}
-        y={y}
-        x={coordinate}
-        color={colors.TextMuted}
-      />
-    </Group>
-  );
-};
 export interface ICoordinatesProps {
   font: SkFont;
   colors: IThemeContext['colors'];
