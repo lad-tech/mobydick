01ac69364bb4ce6de9c0b542e04c02ab2c6300c4
Bibazavr <lebedevki@lad24.ru>
2024-04-26 10:27:49 +0300

    build: update RN to 0.73.7
    Signed-off-by: Bibazavr <lebedevki@lad24.ru>

--- android/app/src/androidTest/java/com/mobydick/DetoxTest.java
+++ android/app/src/androidTest/java/com/mobydick/DetoxTest.java
@@ -1,4 +1,4 @@
-// Replace "com.example" here and below with your app's package name from the top of MainActivity.java
+// Replace "com.example" here and below with your app's package name from the top of MainActivity.kt
 package com.mobydick;
 
 import com.wix.detox.Detox;
