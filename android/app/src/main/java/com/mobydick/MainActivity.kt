33327e7bedb8dbe3ddd50213c763ae32c69d5670
Bibazavr <lebedevki@lad24.ru>
2024-04-26 13:52:13 +0300

    build: return onCreate

--- android/app/src/main/java/com/mobydick/MainActivity.kt
+++ android/app/src/main/java/com/mobydick/MainActivity.kt
@@ -7,6 +7,10 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
