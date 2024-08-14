26bc67e0fbcefe95f9f3b0dda1e5896cf5105394
VadiuninDA <vadiuninda@lad24.ru>
2024-08-14 11:03:46 +0300

    chore: add useAnimatedShake hook

---
+++ packages/core/src/other/hooks/index.ts
@@ -0,0 +1,3 @@
+import useAnimatedShake from './useAnimatedShake';
+
+export {useAnimatedShake};
---
+++ packages/core/src/other/hooks/useAnimatedShake.tsx
@@ -0,0 +1,33 @@
+import {useCallback} from 'react';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
+
+const useAnimatedShake = () => {
+  const shake = useSharedValue<number>(0);
+
+  const animateStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: shake.value}],
+  }));
+
+  const handleShake = useCallback(() => {
+    shake.value = withSequence(
+      withTiming(-5, {duration: 50}),
+      withTiming(5, {duration: 50}),
+      withTiming(0, {duration: 50}),
+    );
+  }, [shake]);
+
+  const childrenShakeElement = (children: Element | Element[]) => (
+    <Animated.View style={animateStyle}>
+      <>{children}</>
+    </Animated.View>
+  );
+
+  return {handleShake, childrenShakeElement, animateStyle};
+};
+
+export default useAnimatedShake;
